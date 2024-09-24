65dc73f3950e050db6c96acf65e655c07cc479b1
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-09-24 17:31:21 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -12,7 +12,14 @@ exports.handler = async function(event, context) {
 
   try {
     const response = await fetch(url);
-    const data = await response.json();
+    //const data = await response.json();
+    let data;
+
+    if (contentType && contentType.includes("application/json")) {
+      data = await response.json();  // Parse as JSON if the response is JSON
+    } else {
+      data = await response.text();  // Otherwise, return as plain text (e.g., HTML)
+    }
 
     return {
       statusCode: 200,
