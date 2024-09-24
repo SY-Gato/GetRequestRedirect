9282907021a63e4ad37790661673984bf221e827
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-09-24 18:03:34 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -16,24 +16,25 @@ exports.handler = async function(event, context) {
     // Get the content type of the response
     const contentType = response.headers.get("content-type");
     console.log(contentType);
-    
-    if (contentType && contentType.includes("image")) {
+
+    if (contentType && contentType.includes("application/json")) {
+      const data = await response.json();  // Parse as JSON
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data)  // Return as JSON
+      };
+    } else if (contentType && contentType.includes("image")) {
       const buffer = await response.buffer();  // Get image data as a buffer
       //const base64Image = buffer.toString('base64');  // Convert to base64
+      const base64version = await btoa(buffer);
       return {
         statusCode: 200,
         headers: {
           "Content-Type": contentType,  // Set correct content type for the response
           "Content-Length": buffer.length
         },
-        body: buffer
+        body: base64version
         //body: base64Image  // Return base64 image data directly
-      };
-    } else if (contentType && contentType.includes("application/json")) {
-      const data = await response.json();  // Parse as JSON
-      return {
-        statusCode: 200,
-        body: JSON.stringify(data)  // Return as JSON
       };
     } else {
       const data = await response.text();  // Get as text for other types
