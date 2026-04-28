6fbc4546f7b626cb5f242aa84b8fcc62ee749603
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2026-04-28 13:37:22 -0500

    Create index.js

---
+++ functions/index.js
@@ -0,0 +1,188 @@
+const axios = require('axios');
+const fetch = require('node-fetch');
+
+// Source - https://stackoverflow.com/a/18650828
+// Posted by anon, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-04-28, License - CC BY-SA 4.0
+
+function formatBytes(a,b=2){if(!+a)return"0 Bytes";const c=0>b?0:b,d=Math.floor(Math.log(a)/Math.log(1024));return`${parseFloat((a/Math.pow(1024,d)).toFixed(c))} ${["Bytes","KiB","MiB","GiB","TiB","PiB","EiB","ZiB","YiB"][d]}`}
+
+
+exports.handler = async function(event, context) {
+  const url = event.queryStringParameters.url;
+  const other = event.queryStringParameters.replacenl;//new URL(event.url).searchParams.has("replacenl");
+  const ip = event.headers["x-forwarded-for"];
+  console.log(other);
+  console.log(ip);
+  if (other == null) {
+    console.log("Not Replacing Newline Characters.");
+  }
+
+  if (!url) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'No URL specified' })
+    };
+  }
+
+  try {
+    // const mxCLength: 99999;
+    const mxCLength = 99999;
+    const mxBodyLen = 99999;
+    
+    // const response = await fetch(url);
+    // const response = await fetch(
+    // const res = await axios.get(
+    // const res = await axios.get(url, {
+    let res;
+    // const useAxios = false
+    const useAxios = true;
+    if (useAxios) {
+      // res = await axios.get(url, {
+      const res1 = await axios.get(url, {
+      timeout: 30000,
+      validateStatus: function (status) {
+        // return stats >= 200 && status < 300;
+        return status >= 200 && status < 300;
+      },
+      //maxContentLength: mxCLength,
+      //maxBodyLength: mxBodyLen,
+      onDownloadProgress: function ({loaded, total, progress, bytes, estimated, rate, download = true}) {
+        /*console.info(`Loaded: ${loaded}
+Total: ${total}
+Prog: ${progress}
+Bytes: ${bytes}
+Estimated: ${estimated}
+Rate: ${rate}
+Download: ${download}`);*/
+        console.info(`Loaded: ${loaded} (${formatBytes(loaded)})
+Total: ${total} (${formatBytes(total)})
+Prog: ${progress}
+Bytes: ${bytes} (${formatBytes(bytes)})
+Estimated: ${estimated}
+Rate: ${rate}
+Download: ${download}`);
+      },
+      signal: new AbortController().signal,
+    });
+      const a11 = res1.headers.get("content-type");
+      /*res = {
+        // headers: res1.headers
+        // headers: res1.header
+        headers: res1.headers,
+        
+      };*/
+      /*res = {
+        // headers: res1.headers
+        // headers: res1.header
+        headers: res1.headers,
+        status: res1.status,
+        text: async () => {
+          
+          // return res1.data;
+          return res1.data;
+        },
+        json: async () => {
+          return JSON.parse(res1.data);
+        },
+      };
+      console.log(res1.data);*/
+      res = res1;
+    } else {
+      res = await fetch(url);
+    }
+      const response = res;
+    //} else {
+    //  res = await fetch(url);
+    //}
+    //const data = await response.json();
+    // const contentType = response.headers.get("content-type")
+    const contentType = response.headers.get("content-type");
+    // let data;
+    // let outData = 
+    // let outBody = {};
+    let outBody = {
+      contentType: contentType,
+      statusCode: response.status,
+      
+    };
+    //} else {
+    //  res = await fetch(url);
+    //}
+
+    /*if (contentType && contentType.includes("application/json")) {
+      // data = await response.json(); // Parse as JSON if the response is JSON
+      outBody.data = await response.json();
+      outBody.isJSON = true;
+    } else {
+      // data = await response.text();  // Otherwise, return as plain text (e.g., HTML)
+      
+    }*/
+
+    // const mainTypes = ["application/json", "text/plain", "text/html"
+    // const mainTypes = ["application/json", "text/plain", "text/html";
+    const mainTypes = ["application/json", "text/plain", "text/html"];
+
+    outBody.isJSON = false;
+    
+    //if (contentType) {
+      /*const ctype = mainTypes.find((type1) => contentType.includes(type1));
+      if (ctype) {
+        switch (ctype) {
+          case "application/json":
+            outBody.isJSON = true;
+            outBody.data = await response.json();
+            break;
+          case "text/plain":
+            outBody.plainText = true;
+            outBody.data = await response.text();
+            break;
+          case "text/html":
+            
+        }
+      }*/
+      /*if (contentType.includes("application/json")) {
+        outBody.isJSON = true;
+        outBody.data = await response.json();
+      }
+    }*/
+    // outBody.data = await response
+    // if (contentType &&.contentType.includes("application/json")) {
+    if (contentType && contentType.includes("application/json")) {
+      outBody.isJSON = true;
+      // outBody.data = await response.json();
+      if (useAxios) {
+        outBody.data = response.data;
+      } else {
+        outBody.data = await response.json();
+      }
+    } else {
+      outBody.isJSON = false;
+      // outBody.data = await response.text();
+      if (useAxios) {
+        outBody.data = response.data;
+      } else {
+        outBody.data = await response.text();
+      }
+    }
+
+    
+
+    /*return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    }; */
+    // return { statusCode
+    return {
+      statusCode: 200,
+      // data: JSON.stringify(outBody)
+      body: JSON.stringify(outBody)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error fetching the URL.', details: error.message || error })
+      //body: JSON.stringify({ error: 'Error fetching the URL.' })
+    };
+  }
+};
